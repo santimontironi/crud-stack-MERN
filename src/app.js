c4d072f08c1593934814744f335dee82e05865d4
santimontironi <santiimontironi@gmail.com>
2025-07-02 19:31:06 -0300

    controllers de auth creados

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import morgan from 'morgan'
+import authRoutes from './routes/auth-routes.js'
 
 const app = express()
 
 app.use(morgan('dev'))
+
+app.use(express.json()) // para parsear JSON en el body de las peticiones
+
+app.use(authRoutes)
 
 export default app
