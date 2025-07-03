77f85dc3b5a9d6a554c5ebc8e50f2a532719bc0e
santimontironi <santiimontironi@gmail.com>
2025-07-03 00:13:05 -0300

    registro de usuario

--- src/controllers/auth-controllers.js
+++ src/controllers/auth-controllers.js
@@ -1,5 +1,24 @@
-export const register = (req,res) => {
-    res.send('REGISTER')
+import User from "../models/user-model"
+
+export const register = async (req,res) => {
+
+    const {username,email,password} = req.body
+
+    try{
+        const newUser = new User({
+            username,
+            email,
+            password
+        })
+    
+        await newUser.save()
+    
+        console.log(newUser)
+    }
+    catch(error){
+        console.log("Wrong user register:",error)
+    }
+    
 }
 
 export const login = (req,res) => {
