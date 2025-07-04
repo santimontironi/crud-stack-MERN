1af6a2426e918351cf522bd08b56c5f2e04a7ded
santimontironi <santiimontironi@gmail.com>
2025-07-04 00:15:24 -0300

    clave hasheada al registrar un usuario

--- src/controllers/auth-controllers.js
+++ src/controllers/auth-controllers.js
@@ -1,17 +1,27 @@
 import User from "../models/user-model.js"
+import bcrypt from "bcryptjs";
 
 export const register = async (req,res) => {
 
     const {username,email,password} = req.body
 
     try{
+
+        const hashPassword = await bcrypt.hash(password,10)
+
         const newUser = new User({
             username,
             email,
-            password
+            password: hashPassword
         })
     
-        await newUser.save()
+        const userSaved = await newUser.save()
+
+        res.json({
+            id:userSaved.id,
+            username: userSaved.username,
+            email: userSaved.email
+        })
         
     }
     catch(error){
--- src/db.js
+++ src/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export const connect = async () => {
     try{
         mongoose.connect("mongodb://localhost/merndb")
-        console.log("DATABASE CONNECTED")
     }
     catch(error){
         console.log("Error in database:",error)
