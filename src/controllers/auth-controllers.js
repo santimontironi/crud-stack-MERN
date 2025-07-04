622e783122a3c788afbfae51f577cf034726184b
santimontironi <santiimontironi@gmail.com>
2025-07-04 00:35:36 -0300

    comentarios agregados

--- src/controllers/auth-controllers.js
+++ src/controllers/auth-controllers.js
@@ -4,28 +4,37 @@ import jwt from "jsonwebtoken";
 
 export const register = async (req,res) => {
 
+    //se toman los datos enviados desde el frontend
     const {username,email,password} = req.body
 
     try{
 
+        //se hashea la contraseña
         const hashPassword = await bcrypt.hash(password,10)
 
+        //se crea un nuevo usuario
         const newUser = new User({
             username,
             email,
             password: hashPassword
         })
     
+        //se guarda el usuario creado
         const userSaved = await newUser.save()
 
+        //creacion del token
         jwt.sign(
+            //payload
             {
                 id:userSaved.id
             },
+            //llave secreta
             "secret123",
+            //opciones
             {
                 expiresIn: "1d"
             },
+            //callback asincrono para almacenar el token en una cookie en el navegador
             (err,token) => {
                 if(err) console.log(err)
                 res.cookie('token',token)
@@ -34,7 +43,7 @@ export const register = async (req,res) => {
                 })
             }
         )
-                
+
     }
     catch(error){
         console.log("Wrong user register:",error)
