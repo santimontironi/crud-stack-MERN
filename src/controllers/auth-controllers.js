fc35cc9756e4e239dafee5809c084256567cd56f
santimontironi <santiimontironi@gmail.com>
2025-07-04 12:20:29 -0300

    new: logout desarrollado

--- src/controllers/auth-controllers.js
+++ src/controllers/auth-controllers.js
@@ -82,3 +82,12 @@ export const login = async (req,res) => {
     }
     
 }
+
+export const logout = (req,res) => {
+    try {
+        res.clearCookie("token");
+        res.status(200).json({ message: "Logout successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Logout error", error: error.message });
+    }
+}
