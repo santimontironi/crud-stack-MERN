bc0c1675ea1af3d6a81b7cdd762e3d1d97654c29
santimontironi <santiimontironi@gmail.com>
2025-07-09 23:33:10 -0300

    referencia de usuario agregada en task-model

--- src/models/task-model.js
+++ src/models/task-model.js
@@ -12,6 +12,11 @@ const taskSchema = new mongoose.Schema({
     date:{
         type:Date,
         default: Date.now
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     }
 },{
     timestamps: true
