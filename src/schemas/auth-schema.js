3c61a83298ed170d52706b896e73b9d82c8bafe6
santimontironi <santiimontironi@gmail.com>
2025-07-10 01:13:58 -0300

    new. se agrego el loginSchema para manejar las validaciones

--- src/schemas/auth-schema.js
+++ src/schemas/auth-schema.js
@@ -1,11 +1,23 @@
 import {z} from "zod";
 
-const registerSchema = z.object({
+export const registerSchema = z.object({
     username: z.string({
         required_error: "Username is required"
     }),
     email: z.string({
         message: "Invalid email"
+    }),
+    password: z.string({
+        required_error: "Password is required"
+    })
+    .min(6,{
+        message:"Password must be at least 6 characters."
+    })
+})
+
+export const loginSchema = z.object({
+    email: z.string({
+        required_error: "Email is required"
     }),
     password: z.string({
         required_error: "Password is required"
