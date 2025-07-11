6c1eeb3dcd3d281d66d5ca4642bdedf521c7c52b
santimontironi <santiimontironi@gmail.com>
2025-07-11 17:27:39 -0300

    task schema agregado

--- src/routes/task-routes.js
+++ src/routes/task-routes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
 import { getTasks, getTaskById, postTask, deleteTask, putTask } from "../controllers/task-controllers.js";
+import { validateSchema } from "../middlewares/validator.js";
+import { taskSchema } from "../schemas/task-schema.js";
 
 const router = Router()
 
-router.get('/tasks', authRequired, getTasks)
-router.get('/task/:id', authRequired, getTaskById)
-router.post('/addTask', authRequired, postTask)
-router.delete('/deleteTask:id', authRequired, deleteTask)
-router.get('/editTask:id', authRequired, putTask)
+router.get('/tasks', validateSchema(taskSchema), authRequired, getTasks)
+router.get('/task/:id', validateSchema(taskSchema), authRequired, getTaskById)
+router.post('/addTask', validateSchema(taskSchema), authRequired, postTask)
+router.delete('/deleteTask:id', validateSchema(taskSchema), authRequired, deleteTask)
+router.get('/editTask:id', validateSchema(taskSchema), authRequired, putTask)
 
 export default router
 
