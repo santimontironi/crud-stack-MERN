d176d471f036c07d3eb044cd38d8287da3fcba0e
santimontironi <santiimontironi@gmail.com>
2025-07-11 17:15:39 -0300

    zod implementado correctamente, funcionan las validaciones

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,9 +1,9 @@
-export const validateSchema = (schema,req,res,next) => {
+export const validateSchema = (schema) => (req,res,next) => {
     try{
-        schema.parse()
+        schema.parse(req.body)
         next()
     }
     catch(error){
-        return res.status(400).send("Error: ",error)
+        return res.status(400).json({error: error.issues.map(i => i.message)})
     }
 }
