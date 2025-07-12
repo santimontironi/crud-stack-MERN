f7069cf4238a6f57b15f450d39cc91d4755b5ba7
santimontironi <santiimontironi@gmail.com>
2025-07-12 16:21:33 -0300

    router configurado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 const App = () => {
   return (
-    <h1 className="text-red-500">APP</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1 className="text-red-500">APP</h1>}/>
+        <Route path="/login" element={<h1 className="text-red-500">LOGIN</h1>}/>
+        <Route path="/register" element={<h1 className="text-red-500">REGISTER</h1>}/>
+        <Route path="/tasks" element={<h1 className="text-red-500">TASKS</h1>}/>
+        <Route path="/addTask" element={<h1 className="text-red-500">ADDTASK</h1>}/>
+        <Route path="/task/:id" element={<h1 className="text-red-500">TASK:ID</h1>}/>
+        <Route path="/profile" element={<h1 className="text-red-500">PROFILE</h1>}/>
+      </Routes>
+      
+    </BrowserRouter>
+    
   )
 }
 
