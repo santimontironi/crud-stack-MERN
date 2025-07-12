b18e5963dc70ef314bbaae43116267a7ace9a7f1
santimontironi <santiimontironi@gmail.com>
2025-07-12 20:05:14 -0300

    estilos en RegisterPage

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -10,12 +10,12 @@ const RegisterPage = () => {
     }
 
     return (
-        <div>
-            <form method="post" onSubmit={handleSubmit(submitForm)}>
-                <input type="text" {...register("username",{required:true})} />
-                <input type="email" {...register("email",{required:true})} />
-                <input type="password" {...register("password",{required:true})} />
-                <button type="submit">Register</button>
+        <div className="contenedorRegister w-full h-screen flex justify-center items-center bg-green-400">
+            <form className="flex flex-col gap-4 border-1 border-white p-5 shadow-[5px_5px_10px_black]" method="post" onSubmit={handleSubmit(submitForm)}>
+                <input className="border-black border-1 w-[300px] p-3 focus:shadow-[2px_2px_5px_black]" type="text" {...register("username",{required:true})} placeholder="Username" />
+                <input className="border-black border-1 w-[300px] p-3 focus:shadow-[2px_2px_5px_black]" type="email" {...register("email",{required:true})} placeholder="Correo electrónico" />
+                <input className="border-black border-1 w-[300px] p-3 focus:shadow-[2px_2px_5px_black]" type="password" {...register("password",{required:true})} placeholder="Contraseña" />
+                <button className="bg-green-800 text-white p-2 cursor-pointer hover:bg-green-950" type="submit">Register</button>
             </form>
         </div>
   )
