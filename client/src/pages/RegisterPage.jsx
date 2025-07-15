fbf008d849f37d741dd8adfe2d36a6777f75a0ca
santimontironi <santiimontironi@gmail.com>
2025-07-15 17:31:04 -0300

    mensajes de errores agregados en RegisterPage

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,10 +6,10 @@ import "../public/css/register.css"
 
 const RegisterPage = () => {
 
-    const {register,handleSubmit,reset} = useForm()
+    const {register,handleSubmit,reset,formState:{ errors }} = useForm()
     const {signUp} = useAuth()
 
-    const[errors,setErrors] = useState("")
+    const[errorRegister,setErrorRegister] = useState("")
     const[correctRegister,setCorrectRegister] = useState(false)
     const navigate = useNavigate()
 
@@ -17,11 +17,11 @@ const RegisterPage = () => {
         try {
           await signUp(values)
           setCorrectRegister(true)
-          setErrors("")
+          setErrorRegister("")
           reset();
         } catch (error) {
             if(error.response?.data?.message){
-                setErrors(error.response.data.message)
+                setErrorRegister(error.response.data.message)
             }
         }
     }
@@ -38,16 +38,25 @@ const RegisterPage = () => {
             
             <div className="contentForm">
                 <form method="post" onSubmit={handleSubmit(submitForm)}>
-                    <input type="text" {...register("username",{required:'El nombre de usuario es obligatorio'})} placeholder="Username" />
-                    <input type="email" {...register("email",{required:'El correo es obligatorio.'})} placeholder="Correo electrónico" />
+                    <input type="text" {...register("username",{required:true})} placeholder="Username" />
+                    <input type="email" {...register("email",{required:true})} placeholder="Correo electrónico" />
                     <input type="password" {...register("password",{required:true,minLength:{value:6,message:'La contraseña debe tener al menos 6 caracteres.'}})} placeholder="Contraseña" />
                     <button type="submit">Registrarse</button>
                 </form>
+                {errors.username && (
+                    <p>El username es requerido</p>
+                )}
+                {errors.email && (
+                    <p>El correo electrónico es requerido</p>
+                )}
+                {errors.password && (
+                    <p>Error: Contraseña vacía o menos de 6 caracteres.</p>
+                )}
                 <p>Si ya tenés cuenta <a href="/login">Ingresá</a></p>
             </div>
             
-            {errors &&(
-                <p className="messageError">{errors}</p>
+            {errorRegister &&(
+                <p className="messageError">{errorRegister}</p>
             )}
             {correctRegister &&(
                 <p className="correctRegister">Usuario registrado correctamente.</p>
