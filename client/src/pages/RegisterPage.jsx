fa255873ca956f893ae009ff8972ed2440b3a0d0
santimontironi <santiimontironi@gmail.com>
2025-07-15 17:42:55 -0300

    estilos en mensajes de error

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -38,20 +38,26 @@ const RegisterPage = () => {
             
             <div className="contentForm">
                 <form method="post" onSubmit={handleSubmit(submitForm)}>
+
                     <input type="text" {...register("username",{required:true})} placeholder="Username" />
+                    {errors.username && (
+                        <p className="error">El username es requerido</p>
+                    )}
+
                     <input type="email" {...register("email",{required:true})} placeholder="Correo electrónico" />
+                    {errors.email && (
+                        <p className="error">El correo electrónico es requerido</p>
+                    )}
+
                     <input type="password" {...register("password",{required:true,minLength:{value:6,message:'La contraseña debe tener al menos 6 caracteres.'}})} placeholder="Contraseña" />
+                    {errors.password && (
+                        <p className="error">Error: Contraseña vacía o menos de 6 caracteres.</p>
+                    )}
+
                     <button type="submit">Registrarse</button>
+
                 </form>
-                {errors.username && (
-                    <p>El username es requerido</p>
-                )}
-                {errors.email && (
-                    <p>El correo electrónico es requerido</p>
-                )}
-                {errors.password && (
-                    <p>Error: Contraseña vacía o menos de 6 caracteres.</p>
-                )}
+                
                 <p>Si ya tenés cuenta <a href="/login">Ingresá</a></p>
             </div>
             
