f30b9262e46ff001bb618a0457e0399cfcb1eda8
santimontironi <santiimontironi@gmail.com>
2025-07-15 11:18:47 -0300

    ajustes responsives

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -33,13 +33,13 @@ const RegisterPage = () => {
 
     return (
         <main className="contenedorRegistro w-full h-screen bg-green-400">
-            <h1 className="text-center md:w-[600px] lg:w-[700px] md:text-[60px] lg:text-[55px] lg:p-0 text-white bg-gray-950 p-3 relative top-[30px] shadow-[6px_6px_10px_black] text-[50px] flex justify-center w-[300px] m-auto">Registro</h1>
+            <h1 className="text-center lg:w-[800px] xl:w-[700px] md:text-[50px] lg:text-[60px] xl:text-[70px] lg:p-0 text-white bg-gray-950 p-3 relative top-[30px] shadow-[6px_6px_10px_black] text-[50px] flex justify-center w-[300px] m-auto">Registro</h1>
             <div className="flex flex-col gap-5 justify-center items-center mt-20">
-                <form className="flex bg-gray-950 flex-col gap-4 p-6 shadow-[8px_8px_12px_black] w-[340px] lg:w-[350px] lg:p-4" method="post" onSubmit={handleSubmit(submitForm)}>
-                    <input className="bg-white w-[292px] lg:w-[318px] lg:text-[10px] p-2 focus:shadow-[5px_5px_5px_black]" type="text" {...register("username",{required:'El nombre de usuario es obligatorio'})} placeholder="Username" />
-                    <input className="bg-white w-[292px] lg:w-[318px] lg:text-[10px] p-2 focus:shadow-[5px_5px_5px_black]" type="email" {...register("email",{required:'El correo es obligatorio.'})} placeholder="Correo electrónico" />
-                    <input className="bg-white w-[292px] lg:w-[318px] lg:text-[10px] p-2 focus:shadow-[5px_5px_5px_black]" type="password" {...register("password",{required:true,minLength:{value:6,message:'La contraseña debe tener al menos 6 caracteres.'}})} placeholder="Contraseña" />
-                    <button className="bg-green-600 text-white p-2 cursor-pointer hover:bg-green-700 lg:text-[10px]" type="submit">Registrarse</button>
+                <form className="flex bg-gray-950 flex-col gap-4 p-6 shadow-[8px_8px_12px_black] w-[340px] lg:w-[350px] xl-[400px] lg:p-4" method="post" onSubmit={handleSubmit(submitForm)}>
+                    <input className="bg-white w-[292px] lg:w-[318px] xl:w-[318px] lg:text-[10px] xl:text-[13px] p-2 focus:shadow-[5px_5px_5px_black]" type="text" {...register("username",{required:'El nombre de usuario es obligatorio'})} placeholder="Username" />
+                    <input className="bg-white w-[292px] lg:w-[318px] xl:w-[318px] lg:text-[10px] xl:text-[13px] p-2 focus:shadow-[5px_5px_5px_black]" type="email" {...register("email",{required:'El correo es obligatorio.'})} placeholder="Correo electrónico" />
+                    <input className="bg-white w-[292px] lg:w-[318px] xl:w-[318px] lg:text-[10px] xl:text-[13px] p-2 focus:shadow-[5px_5px_5px_black]" type="password" {...register("password",{required:true,minLength:{value:6,message:'La contraseña debe tener al menos 6 caracteres.'}})} placeholder="Contraseña" />
+                    <button className="bg-green-600 text-white p-2 cursor-pointer hover:bg-green-700 lg:text-[10px] xl:text-[13px]" type="submit">Registrarse</button>
                 </form>
                 <p className="p-3 bg-white lg:text-[12px] lg:p-1">Si ya tenés cuenta <a className="underline text-green-700 font-bold" href="/login">Ingresá</a></p>
             </div>
